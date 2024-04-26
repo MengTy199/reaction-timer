d042d7fd0c064089438077730bc1e6366557171f
MengTy199 <117325886+MengTy199@users.noreply.github.com>
2024-04-26 09:05:43 +0700

    ls

---
+++ src/components/Results.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>Reaction time:  {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  name: "app",
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Mengty Fingers";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Snail pace ...";
+    }
+  },
+};
+</script>
+
+<style></style>
